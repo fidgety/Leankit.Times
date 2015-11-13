0cc18b6d383b840a4a76493e1ab365672d84f94d
Alex Meah <alexmeahdotcom@gmail.com>
2015-11-13 16:00:49 +0000

    Refactor featureCards

--- middleware/featureCards.js
+++ middleware/featureCards.js
@@ -1,24 +1,18 @@
-var _ = require('lodash');
-
 module.exports = function createFeatureParser(config, client) {
-
     return function storeFeatures(req, res, next) {
         client.getBoardArchiveCards(config.boardId, function (err, archive) {
             if (err) {
-                console.log(err);
                 return next(err)
             }
-            var data = [];
-            _.forEach(config.cardTypes, function (cardType) {
-                var cardsOfType = _.filter(archive, {
-                    TypeName: cardType
-                });
-                data = data.concat(cardsOfType);
+
+            var featureCards = [];
+
+            var featureCards = archive.filter(function(card) {
+                return config.cardTypes.indexOf(card.TypeName) > -1;
             });
 
-            req.features = data;
+            req.features = featureCards;
             next();
         });
     }
-
 }
