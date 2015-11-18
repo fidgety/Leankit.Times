9dc258f5a2582235cafb4911101e24bf6d2e5f7b
Alex Perry <alexperry2@gmail.com>
2015-11-18 16:49:42 +0000

    Refactored cardTimes further

--- middleware/cardTimes.js
+++ middleware/cardTimes.js
@@ -1,54 +1,46 @@
 var _ = require('lodash');
 var moment = require('moment');
 var momentBusiness = require('moment-business');
+var Promise = require('bluebird');
 
 var config = require('../config/config');
 var leankit = require("leankit-client");
 var client = leankit.createClient(config.accountName, config.email, config.password);
 var dateFormat = 'DD/MM/YYYY at hh:mm:ss a';
 
+var getCardHistory = Promise.promisify(client.getCardHistory, {context: client});
+
 module.exports = function cardTimes(req, res, next) {
-    var called = 0;
     var featureWithSize = req.features.filter(function (card) {
         return card.Size !== 0;
     });
 
-    var numberOfCards = featureWithSize.length;
+    Promise.all(featureWithSize.map(function (card) {
+        return getCardHistory(config.boardId, card.Id)
+            .then(function (cardHistory) {
+                var cardHistorySortedByTime = cardHistory.map(function (change) {
+                    change.Date = moment(change.DateTime, dateFormat).format('X');
 
-    _.forEach(featureWithSize, function (card, i) {
-        client.getCardHistory(config.boardId, card.Id, function (err, cardHistory) {
-            called++;
-            if (err) {
-                console.log('err', err)
-            }
+                    return change;
+                }).sort(function(a, b) {
+                    return parseInt(a.Date, 10) - parseInt(b.Date, 10);
+                });
 
-            var cardHistorySortedByTime = cardHistory.map(function (change) {
-                change.Date = moment(change.DateTime, dateFormat).format('X');
+                var start = _.find(cardHistorySortedByTime, {ToLaneTitle: config.fromLane});
+                var end = _.find(cardHistorySortedByTime.reverse(), {ToLaneTitle: config.toLane});
 
-                return change;
-            }).sort(function(a, b) {
-                return parseInt(a.Date, 10) - parseInt(b.Date, 10);
-            });
+                if (!start || !end) {
+                    return false;
+                }
 
-            var start = _.find(cardHistorySortedByTime, {ToLaneTitle: config.fromLane});
-            var end = _.find(cardHistorySortedByTime.reverse(), {ToLaneTitle: config.toLane});
+                firstTimeWipped = moment(start.DateTime, dateFormat);
+                lastTimeDone = moment(end.DateTime, dateFormat);
 
-            if (!start || !end) {
-                return false;
-            }
-
-            firstTimeWipped = moment(start.DateTime, dateFormat);
-            lastTimeDone = moment(end.DateTime, dateFormat);
-
-            var totalTime = firstTimeWipped.weekDays(lastTimeDone);
-            card.TimeInDays = totalTime;
+                card.TimeInDays = firstTimeWipped.weekDays(lastTimeDone);
         });
-    });
-    var interval = setInterval(function () {
-        if (called === numberOfCards) {
-            clearInterval(interval);
-            next();
-        }
-    }, 1000);
-
+    }))
+    .then(function (data) {
+        next();
+    })
+    .catch(next);
 };
